3728f135ebab7d82d26ebd3a5e32ab1c8c6ae16d
Amisha65 <amishadh00@gmail.com>
2025-05-01 02:32:52 +0530

    Resolve SignUp error

--- src/componants/SignUp.jsx
+++ src/componants/SignUp.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import css from "./signup.module.css";
+import css from "./signUp.module.css";
 import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
